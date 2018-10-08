424482ed1cb2e5522dc875a8b7016a1b3d3a0c05
Fabricia <fabricia.sales@zydax.com.br>
2018-10-08 13:19:27 -0300

    Consulta de cep

---
+++ ViaCEP/ViaCEP/ViaCEP/MainPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using ViaCEP.Servico.Modelo;
+using ViaCEP.Servico;
+
+namespace ViaCEP
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            BOTAO.Clicked += BuscarCEP;
+        }
+
+        private void BuscarCEP(object sender, EventArgs args)
+        {
+
+            //  Lógica do programa
+            string cep = CEP.Text.Trim();
+
+            if (isValidCEP(cep))
+            {
+                Endereco end = ViaCEPServico.BuscarEnderecoViaCEP(cep);
+
+                RESULTADO.Text = string.Format("Endereço: {0} - {1},{2},{3}", end.logradouro, end.bairro, end.localidade, end.uf);
+            }
+        }
+
+        //  Validações
+        private bool isValidCEP(string cep)
+        {
+            bool valido = true;
+
+            if (cep.Length != 8)
+            {
+                DisplayAlert("Erro", "CEP Inválido! O CEP deve conter 8 caracteres.", "OK");
+                valido = false;
+            }
+
+            int NovoCEP = 0;
+            if (!int.TryParse(cep, out NovoCEP))
+            {
+                DisplayAlert("Erro", "CEP Inválido! O CEP deve conter apenas números.", "OK");
+                valido = false;
+            }
+
+            return valido;
+        }
+    }
+}
---
+++ ViaCEP/ViaCEP/ViaCEP/ViaCepServico.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using ViaCEP.Servico.Modelo;
+using Newtonsoft.Json;
+
+namespace ViaCEP
+{
+    public class ViaCEPServico
+    {
+        private static string EndereçoURL = "https://viacep.com.br/ws/{0}/json/";
+
+        public static Endereco BuscarEnderecoViaCEP(string cep)
+        {
+            string NovoEnderecoURL = string.Format(EndereçoURL, cep);
+
+            WebClient wc = new WebClient();
+            string Conteudo = wc.DownloadString(NovoEnderecoURL);
+
+            Endereco end = JsonConvert.DeserializeObject<Endereco>(Conteudo);
+
+            return end;
+        }
+    }
+}
